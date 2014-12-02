1798b31c0d712293738a370e6013186ea8ebcc8d
Dobromir Ivanov <dobromirivanov1@gmail.com>
2014-12-03 00:21:30 +0200

    Admin progress

---
+++ Application.Web/Areas/Administration/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Application.Web.Areas.Administration.InputModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Application.Web.Areas.Administration.Controllers
+{
+    public class ProductsController : BaseController
+    {
+        // GET: Administration/Products
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var model = new CreateProductInputModel();
+            return this.View(model);
+        }
+    }
+}
---
+++ Application.Web/Areas/Administration/InputModels/CreateProductInputModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Application.Web.Areas.Administration.InputModels
+{
+    public class CreateProductInputModel
+    {
+        [Required]
+        [Display(Name = "Име:")]
+        public string Title { get; set; }
+
+        [Required]
+        [AllowHtml]
+        [Display(Name = "Кратко описание:")]
+        [DataType("tinymce_full")]
+        [UIHint("tinymce_full")]
+        public string ShortDescription { get; set; }
+
+        [Required]
+        [AllowHtml]
+        [Display(Name = "Дълго описание:")]
+        [DataType("tinymce_full")]
+        [UIHint("tinymce_full")]
+        public string LongDescription { get; set; }
+
+        [Required]
+        [AllowHtml]
+        [Display(Name = "Булети:")]
+        [DataType("tinymce_full")]
+        [UIHint("tinymce_full")]
+        public string BulletsText { get; set; }
+
+        // TODO: Create custon validation for the tags
+        [Required]
+        [Display(Name = "Tags")]
+        public string Tags { get; set; }
+    }
+}
