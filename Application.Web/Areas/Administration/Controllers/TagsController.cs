4f9fba671e86f466ffe97af4e86bb5959da30369
nzhul <dobromirivanov1@gmail.com>
2015-01-12 23:04:00 +0200

    Delete tags from admin panel

---
+++ Application.Web/Areas/Administration/Controllers/TagsController.cs
@@ -0,0 +1,34 @@
+﻿using Application.Web.Areas.Administration.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Application.Web.Areas.Administration.Controllers
+{
+    public class TagsController : BaseController
+    {
+        // GET: Administration/Tags
+        public ActionResult Index()
+        {
+            var tagsList = this.Data.Tags.All()
+                .OrderByDescending(x => x.Id)
+                .AsEnumerable()
+                .Select(x => new TagViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name
+                }).ToList();
+            return View(tagsList);
+        }
+
+        [HttpPost]
+        public ActionResult DeleteTag(int id)
+        {
+            this.Data.Tags.Delete(id);
+            this.Data.SaveChanges();
+            return Content("<span class='label label-success'>Изтрито успешно!</span>");
+        }
+    }
+}
---
+++ Application.Web/Areas/Administration/Models/ViewModels/TagViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Application.Web.Areas.Administration.Models.ViewModels
+{
+    public class TagViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
