977c1316271f89cfccb5e698b7bb22af9d2d36d7
nzhul <dobromirivanov1@gmail.com>
2015-01-23 00:02:53 +0200

    New DropDown

--- Application.Web/Areas/Administration/Controllers/BaseController.cs
+++ Application.Web/Areas/Administration/Controllers/BaseController.cs
@@ -32,6 +32,35 @@ namespace Application.Web.Areas.Administration.Controllers
                 })
                 .ToList();
             ViewBag.LayoutModel = model;
+
+            // Populate Categories - TODO: use Cache
+            model.Categories = GetAllCategories();
+            ViewBag.LayoutModel = model;
+        }
+
+        private IEnumerable<CategoryViewModel> GetAllCategories()
+        {
+            return this.Data.Categories
+                .All()
+                .OrderBy(x => x.DisplayOrder)
+                .AsEnumerable()
+                .Select(x => new CategoryViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Description = x.Description,
+                    SubCategories = this.Data.SubCategories
+                        .All()
+                        .OrderBy(y => y.DisplayOrder)
+                        .Where(y => y.CategoryId == x.Id)
+                        .Select(y => new SubCategoryViewModel
+                        {
+                            Id = y.Id,
+                            Name = y.Name,
+                            Description = y.Description
+                        })
+                }).ToList();
+
         }
     }
 }
